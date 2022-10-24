f13948469bbfa5ba12a85715dd6390572d0f2d63
Clément DOUIN <clement.douin@posteo.net>
2022-10-24 10:01:44 +0200

    fix args sent from telescope to email#list_with

--- lua/himalaya/folder/pickers/telescope.lua
+++ lua/himalaya/folder/pickers/telescope.lua
@@ -11,7 +11,7 @@ local function preview_command(entry, bufnr)
   vim.api.nvim_buf_call(bufnr, function()
     local page = 0 -- page 0 for preview
     local account = vim.fn['himalaya#domain#account#current']()
-    local success, output = pcall(vim.fn['himalaya#domain#email#list_with'], account, entry.value, page, true)
+    local success, output = pcall(vim.fn['himalaya#domain#email#list_with'], account, entry.value, page)
     if not (success) then
       vim.cmd('redraw')
       vim.bo.modifiable = true
